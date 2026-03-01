ce595b0eb2d72805d30f97c93958008117687857
mispusinov <mispusinov@diasoft.ru>
2026-03-01 17:10:29 +0300

    Исправленное домашнее задание к лекции 2.5

--- Main.java
+++ Main.java
@@ -1,23 +1,31 @@
 public class Main {
 
-    static class MyArraySizeException extends RuntimeException {
+    static class MyArraySizeException extends Exception {
         public MyArraySizeException(String message) {
             super(message);
         }
     }
 
-    static class MyArrayDataException extends RuntimeException {
+    static class MyArrayDataException extends Exception {
         public MyArrayDataException(String message) {
             super(message);
         }
     }
 
-    public static int processArray(String[][] array) {
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+
+        if (array == null) {
+            throw new MyArraySizeException("Массив равен null");
+        }
+
         if (array.length != 4) {
             throw new MyArraySizeException("Неправильное количество строк: " + array.length);
         }
 
         for (int i = 0; i < 4; i++) {
+            if (array[i] == null) {
+                throw new MyArraySizeException("Строка " + i + " равна null");
+            }
             if (array[i].length != 4) {
                 throw new MyArraySizeException("В строке " + i + " неправильное количество столбцов: " + array[i].length);
             }
@@ -26,6 +34,9 @@ public class Main {
         int sum = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
+                if (array[i][j] == null) {
+                    throw new MyArrayDataException("Ошибка в ячейке [" + i + "][" + j + "]: значение null");
+                }
                 try {
                     sum += Integer.parseInt(array[i][j]);
                 } catch (NumberFormatException e) {
@@ -37,12 +48,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String[][] array = {
-                {"1", "2", "3", "4"},
-                {"5", "6", "7", "8"},
-                {"9", "10", "11", "12"},
-                {"13", "14", "15", "16"}
-        };
+        String[][] array = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
 
         try {
             int result = processArray(array);
